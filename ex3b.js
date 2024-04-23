9d7333913420b9de0cb6970818684cb282302c5c
dnlslcd <danielsalcedoq@gmail.com>
2024-04-23 20:31:37 +0200

    ex3b hecho

--- ex3b.js
+++ ex3b.js
@@ -8,9 +8,16 @@ function isPositive(num) {
 
 /**
  * 1. ¿Que nombre tiene esta función?
+ * "isPositive"
+ * 
  * 2. ¿Que deberíamos escribir para ejecutar esta función?
+ * "isPositive(6);
+ * "
  * 3. ¿Qué tipo de datos devuelve esta función? ¿Un string, un number o un boolean?
+ * boolean
+ * 
  * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué?
+ * no es correcto porque el nombre no representa con bastante fidelidad el valor de la función?
  */
 
 let x = 4;
